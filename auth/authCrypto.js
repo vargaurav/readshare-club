b8b65246ba1194b9eeda0eb7705efd0ea222ec6b
Gaurav Kumar <gaurav19.kumar@paytm.com>
2019-12-25 18:02:13 +0530

    Lock on auth

--- auth/authCrypto.js
+++ auth/authCrypto.js
@@ -1,29 +1,3 @@
-'use strict'
-const crypto = require('crypto');
-let key = "bdjbdvudvujbxjxbdwjdwjhwjsbxjsxjsxsjxsjjhjwuuwjhbxu";
-function encrypt(text, callback){
-    try{
-        var cipher = crypto.createCipher('aes-256-cbc',key)
-        var crypted = cipher.update(text,'utf8','hex')
-        crypted += cipher.final('hex');
-        callback(null, crypted);
-    } catch(ex) {
-        callback(ex);
-    }
-};
-  
-function decrypt(text, callback){
-  try{
-      var decipher = crypto.createDecipher('aes-256-cbc',key)
-      var dec = decipher.update(text,'hex','utf8')
-      dec += decipher.final('utf8');
-      callback(null, dec);
-  } catch(ex) {
-      callback(ex);
-  }
-};
-
-module.exports = {
-    encrypt,
-    decrypt
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:d04db99c2213153d261a28fc7204996af641749325befeb320af4629ef052b79
+size 703
